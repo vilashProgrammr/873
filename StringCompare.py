e23ae1a4d451e909e3ba965a8a181d1693c20684
candidate <candidate@programmr.com>
2019-09-23 07:18:33 +0000

    FINISH TEST

--- StringCompare.py
+++ StringCompare.py
@@ -6,10 +6,10 @@ def main():
     
 def compareStrings(s1, s2):
     #Uncomment the below lines
-    
-    #if s1==s2:
-    #   print("strings are equal")
-    #else:
-    #   print("strings are not equal")
+
+    if s1==s2:
+       print("strings are equal")
+    else:
+       print("strings are not equal")
     
 main()
